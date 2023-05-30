ac1317abdc2f0cab9da0642186eddc3e868c20dc
Stanislav <76952027+Av3lle@users.noreply.github.com>
2023-05-30 11:41:13 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,10 @@ if [ "$(id -u)" -eq 0 ]; then
     exit 1
 fi
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
-cd "${CURRENT_DIR}" || exit
+
+dirr="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+echo "dir = '$dirr/fedora.sh'" > "$dirr"/current_dir.py
+cd "${dirr}" || exit
+
 
 python3 fedora.py
