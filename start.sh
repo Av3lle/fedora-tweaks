14f06c8a382f321bbf6eef8df91faf5b9b3a11aa
Stanislav <76952027+Av3lle@users.noreply.github.com>
2023-05-30 10:49:06 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [[ $EUID -ne 1 ]]; then
+if [ "$(id -u)" -eq 0 ]; then
     echo "Скрипт не должен быть запущен от имени пользователя root!"
     exit 1
 fi
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
-cd "${CURRENT_DIR}"
+cd "${CURRENT_DIR}" || exit
 
 python3 fedora.py
