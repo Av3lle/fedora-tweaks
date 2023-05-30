3b53ef7e8a22f1d63bb2cf557ee44cf3b4a2994e
Stanislav <76952027+Av3lle@users.noreply.github.com>
2023-05-30 11:40:56 +0300

    fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -82,11 +82,9 @@ disable_services() {
 
 
 aliases() {
-  ALIAS=$'alias n="neofetch\nalias k="uname -rs"\nalias g="gnome-shell --version"\nalias f="lsb_release -sd"\nalias m="inxi -G |grep Mesa"\n \
-  alias age="stat / | grep "Birth""\nalias ram="sudo dmidecode -t memory | grep Speed"\nalias cpu="lscpu | grep Model"\n \
-  alias cpuc="lscpu"\nalias w="wine --version"\nalias pc="inxi -Ixxx"\nalias net="inxi -Nxxx"' \
-  sudo echo $ALIAS >> ~/.bashrc
-  waitt
+  echo $'\nalias n="neofetch\nalias k="uname -rs"\nalias g="gnome-shell --version"\nalias f="lsb_release -sd"\nalias m="inxi -G |grep Mesa"
+  \nalias age="stat / | grep "Birth""\nalias ram="sudo dmidecode -t memory | grep Speed"\nalias cpu="lscpu | grep Model"
+  \nalias cpuc="lscpu"\nalias w="wine --version"\nalias pc="inxi -Ixxx"\nalias net="inxi -Nxxx"' >> ~/.bashrc 
 }
 
 
